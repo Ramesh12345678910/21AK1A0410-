dda3db1fddfc131bea1f234b1ed97036026d77da
palisetti <palisettibhaskarrameshsai@gmail.com>
2025-01-31 12:03:26 +0530

    the updatd version

--- src/main/java/com/mphasis/bhaskar/App.java
+++ src/main/java/com/mphasis/bhaskar/App.java
@@ -6,8 +6,17 @@ package com.mphasis.bhaskar;
  */
 public class App 
 {
+	public void addData(String data)
+	{
+		System.out.print("Data"+data);
+		if(data.equals("bhaskar"))
+		{
+			System.out.println("Welcome");
+		}
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("everyone");
     }
 }
